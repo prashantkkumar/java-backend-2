c6eca957dc19523cc21fddd757212dcff2547ce8
Prashant <prashantoff61@gmail.com>
2025-04-26 15:06:12 +0530

    [Prashant] add. Deleting

--- demo/src/main/java/com/example/demo/Bean/departmentBean.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Bean;
-
-import org.springframework.stereotype.Component;
-@Component
-public class departmentBean {
-    public String getDeptName() {
-        return "IT";
-    }
-}
